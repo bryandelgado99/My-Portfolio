dd9bd2c591caeeef48d87bed3f81f454a92c0f02
Bryan Delgado <bryand9970@gmail.com>
2024-09-16 19:49:38 -0500

    Incorporación de configuraciones para despliegue con Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 3000,
+  },
 })
