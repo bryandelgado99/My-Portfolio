f9f5f6a9122b4bf43beda21644663ac0f24397f5
Bryan Delgado <bryand9970@gmail.com>
2024-09-16 19:59:12 -0500

    Agregado de animaciones para desplazamiento, control de scroll y enrutamiento

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,6 +17,7 @@ function Navbar() {
     const sections = ['inicio', 'acerca', 'habilidades', 'proyectos', 'contacto'];
 
     // Función para manejar el scroll y actualizar el estado
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const handleScroll = () => {
         let currentSection = '';
         const scrollTop = window.scrollY;
@@ -61,50 +62,51 @@ function Navbar() {
     return (
         <div className={`flex justify-center items-center w-full h-auto`}>
             <nav
-                className={`fixed sm:top-0 sm:h-fit sm:rounded-full rounded-t-3xl sm:p-2 p-3 sm:mx-auto bottom-0 ${isScrolled ? 'sm:w-full sm:rounded-none sm:my-0 sm:justify-evenly sm:py-3' : 'sm:w-fit sm:my-8'} bg-indigo-400 text-white shadow-lg flex transition-all duration-300 ease-in-out justify-around space-x-2`}>
+                className={`fixed sm:top-0 sm:h-fit sm:rounded-full rounded-t-3xl sm:p-2 p-3 sm:mx-auto bottom-0 ${isScrolled ? 'sm:w-full sm:rounded-none sm:my-0 sm:justify-evenly sm:py-3' : 'sm:w-fit sm:my-8'} bg-indigo-400 text-white shadow-lg flex transition-all duration-300 ease-in-out justify-around sm:space-x-2`}>
+
                 {/* Botón de Inicio */}
                 <button
-                    className={`flex items-center ${selected === 'inicio' ? 'bg-white text-indigo-400 animate__animated animate__fadeIn' : 'hover:bg-indigo-500'} px-5 py-4 rounded-full transition duration-300 ease-in-out`}
+                    className={`flex flex-col justify-center items-center sm:flex-row ${selected === 'inicio' ? 'bg-white text-indigo-400 animate__animated animate__fadeIn' : 'hover:bg-indigo-500'} gap-1 px-5 py-4 rounded-full transition duration-300 ease-in-out`}
                     onClick={() => handleClick('inicio')}
                 >
                     <FontAwesomeIcon icon={faHouseChimney} className="text-base sm:text-md"/>
-                    <span className={`ml-2 ${selected === 'inicio' ? 'block' : 'hidden'} sm:block text-sm sm:text-md`}>Inicio</span>
+                    <span className={`${selected === 'inicio' ? 'block' : 'hidden'} sm:block text-xs sm:text-md`}>Inicio</span>
                 </button>
 
                 {/* Botón de Acerca De */}
                 <button
-                    className={`flex items-center ${selected === 'acerca' ? 'bg-white text-indigo-400 animate__animated animate__fadeIn' : 'hover:bg-indigo-500'} px-5 py-4 rounded-full transition duration-300 ease-in-out`}
+                    className={`flex flex-col justify-center items-center sm:flex-row ${selected === 'acerca' ? 'bg-white text-indigo-400 animate__animated animate__fadeIn' : 'hover:bg-indigo-500'} gap-1 px-5 py-4 rounded-full transition duration-300 ease-in-out`}
                     onClick={() => handleClick('acerca')}
                 >
                     <FontAwesomeIcon icon={faAddressCard} className="text-base sm:text-md"/>
-                    <span className={`ml-2 ${selected === 'acerca' ? 'block' : 'hidden'} sm:block text-sm sm:text-md`}>Sobre Mí</span>
+                    <span className={`${selected === 'acerca' ? 'block' : 'hidden'} sm:block text-xs sm:text-md`}>Sobre Mí</span>
                 </button>
 
                 {/* Botón de Habilidades */}
                 <button
-                    className={`flex items-center ${selected === 'habilidades' ? 'bg-white text-indigo-400 animate__animated animate__fadeIn' : 'hover:bg-indigo-500'} px-5 py-4 rounded-full transition duration-300 ease-in-out`}
+                    className={`flex flex-col justify-center items-center sm:flex-row ${selected === 'habilidades' ? 'bg-white text-indigo-400 animate__animated animate__fadeIn' : 'hover:bg-indigo-500'} gap-1 px-5 py-4 rounded-full transition duration-300 ease-in-out`}
                     onClick={() => handleClick('habilidades')}
                 >
                     <FontAwesomeIcon icon={faLightbulb} className="text-base sm:text-md"/>
-                    <span className={`ml-2 ${selected === 'habilidades' ? 'block' : 'hidden'} sm:block text-sm sm:text-md`}>Habilidades</span>
+                    <span className={`${selected === 'habilidades' ? 'block' : 'hidden'} sm:block text-xs sm:text-md`}>Habilidades</span>
                 </button>
 
                 {/* Botón de Proyectos */}
                 <button
-                    className={`flex items-center ${selected === 'proyectos' ? 'bg-white text-indigo-400 animate__animated animate__fadeIn' : 'hover:bg-indigo-500'} px-5 py-4 rounded-full transition duration-300 ease-in-out`}
+                    className={`flex flex-col justify-center items-center sm:flex-row ${selected === 'proyectos' ? 'bg-white text-indigo-400 animate__animated animate__fadeIn' : 'hover:bg-indigo-500'} gap-1 px-5 py-4 rounded-full transition duration-300 ease-in-out`}
                     onClick={() => handleClick('proyectos')}
                 >
                     <FontAwesomeIcon icon={faBriefcase} className="text-base sm:text-md"/>
-                    <span className={`ml-2 ${selected === 'proyectos' ? 'block' : 'hidden'} sm:block text-sm sm:text-md`}>Proyectos</span>
+                    <span className={`${selected === 'proyectos' ? 'block' : 'hidden'} sm:block text-xs sm:text-md`}>Proyectos</span>
                 </button>
 
                 {/* Botón de Contacto */}
                 <button
-                    className={`flex items-center ${selected === 'contacto' ? 'bg-white text-indigo-400 animate__animated animate__fadeIn' : 'hover:bg-indigo-500'} px-5 py-4 rounded-full transition duration-300 ease-in-out`}
+                    className={`flex flex-col justify-center sm:flex-row items-center ${selected === 'contacto' ? 'bg-white text-indigo-400 animate__animated animate__fadeIn' : 'hover:bg-indigo-500'} gap-1 px-5 py-4 rounded-full transition duration-300 ease-in-out`}
                     onClick={() => handleClick('contacto')}
                 >
                     <FontAwesomeIcon icon={faEnvelopeOpenText} className="text-base sm:text-md"/>
-                    <span className={`ml-2 ${selected === 'contacto' ? 'block' : 'hidden'} sm:block text-sm sm:text-md`}>Contacto</span>
+                    <span className={`${selected === 'contacto' ? 'block' : 'hidden'} sm:block text-xs sm:text-md`}>Contacto</span>
                 </button>
             </nav>
         </div>
