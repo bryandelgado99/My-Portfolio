d4c73715f0d3f451bef29545a4bad107b3fdb987
Bryan Paul Delgado <bryand9970@gmail.com>
2024-09-24 00:45:33 -0500

    Creación de las tarjeas para cada proyecto, de forma generalizada, para poder definir luego la iteración de tarjetas y su generación en base a la cantidad de proyecto y sus tipos. En este caso, se ha implementado para la vista móvil.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -11,7 +11,9 @@ import { Link } from 'react-router-dom';
 
 function ProjectCard() {
   return (
-      <article className='hidden w-auto h-auto sm:flex justify-between gap-8 rounded-3xl bg-white shadow-xl'>
+    <>
+      {/*Tarjeta para dispositivos grandes */}
+      <article className='hidden w-auto h-auto tablet:flex justify-between gap-4 rounded-3xl bg-white shadow-xl'>
         <div className='py-8 ml-8'>
           <h2 className='text-xl font-label font-bold'>Electrónica Zurita</h2>
           <p className='text-base text-gray-700 mt-3'>
@@ -35,28 +37,74 @@ function ProjectCard() {
 
         </div>
         <div className='relative bg-slate-500 rounded-2xl shadow-2xl'>
-          <img src={ElectronicaIMG} className='w-[400px] h-full object-cover rounded-2xl'/>
+          <img src={ElectronicaIMG} className='w-[400px] h-full object-cover rounded-2xl' />
 
           <div className='absolute flex justify-around items-center bottom-5 w-full'>
             <md-filled-tonal-icon-button>
-              <FontAwesomeIcon icon={faFlutter} className='text-xl'/>
+              <FontAwesomeIcon icon={faFlutter} className='text-xl' />
             </md-filled-tonal-icon-button>
             <md-filled-tonal-icon-button>
-              <FontAwesomeIcon icon={faDartLang} className='text-xl'/>
+              <FontAwesomeIcon icon={faDartLang} className='text-xl' />
             </md-filled-tonal-icon-button>
             <md-filled-tonal-icon-button>
-              <FontAwesomeIcon icon={faNodeJs} className='text-xl'/>
+              <FontAwesomeIcon icon={faNodeJs} className='text-xl' />
             </md-filled-tonal-icon-button>
           </div>
         </div>
       </article>
+
+      {/* Tarjeta para dispsoitivos móviles */}
+      <article className='tablet:hidden flex w-auto h-auto flex-col justify-between gap-6 rounded-3xl bg-white shadow-2xl'>
+          {/* Imagen */}
+          <div className='relative rounded-2xl shadow-md'>
+            <img src={ElectronicaIMG} className='w-full h-[350px] object-cover rounded-t-2xl' />
+
+            <div className='absolute flex justify-around items-center bottom-5 w-full'>
+              <md-filled-tonal-icon-button>
+                <FontAwesomeIcon icon={faFlutter} className='text-xl' />
+              </md-filled-tonal-icon-button>
+              <md-filled-tonal-icon-button>
+                <FontAwesomeIcon icon={faDartLang} className='text-xl' />
+              </md-filled-tonal-icon-button>
+              <md-filled-tonal-icon-button>
+                <FontAwesomeIcon icon={faNodeJs} className='text-xl' />
+              </md-filled-tonal-icon-button>
+          </div>
+        </div>
+
+        <div className='px-8'>
+          <h2 className='text-xl font-label font-bold'>Electrónica Zurita</h2>
+          <p className='text-base text-gray-700 mt-3 text-justify'>
+            Una aplicación que permite la gestión de equipos y órdenes de trabajo para distintos servicios electrónicos.
+          </p>
+          <aside className='flex flex-col py-10 mb-4 gap-8 justify-start items-center w-full'>
+            <Link to="https://github.com/bryandelgado99/Electronica-Zurita--Mobile-App">
+              <md-filled-tonal-button className="">
+                <FontAwesomeIcon icon={faGithub} className='text-base'/>
+                <span className='ml-8 text-base'  >Repositorio</span>
+              </md-filled-tonal-button>
+            </Link>
+
+            <Link to="https://play.google.com/store/apps/details?id=com.pauldev.electronica_zurita">
+              <md-filled-button className="">
+                <FontAwesomeIcon icon={faGooglePlay} className='text-base'/>
+                <span className='ml-3 text-base'>Ver en Play Store</span>
+              </md-filled-button>
+            </Link>
+          </aside>
+
+        </div>
+        
+      </article>
+      
+    </>
   );
 }
 
 // Componente contenedor para manejar múltiples tarjetas
 function ProjectCardContainer() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 overflow-y-scroll h-auto p-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 tablet:grid-cols-1 lg:grid-cols-3 gap-8 overflow-y-scroll h-auto p-8">
       {/* Instancias de ProjectCard */}
       <ProjectCard />
       <ProjectCard />
